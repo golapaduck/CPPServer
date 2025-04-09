900fb7afd5b1b48f53bfb518acffd014d577b9ef
golapaduck <golapaduck29@naver.com>
2025-04-09 18:06:52 +0900

    [Feat] : 클라이언트 메세지

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,63 +1,35 @@
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <windows.h>
 #include <iostream>
 
-#pragma comment(lib, "Ws2_32.lib")
+#include <olc_net.h>
 
-using namespace std;
+enum class CustomMsgTypes : uint32_t
+{
+	FireBullet,
+	MovePlayer
+};
 
-int main() {
+int main()
+{
+	olc::net::message<CustomMsgTypes> msg;
+	msg.header.id = CustomMsgTypes::FireBullet;
 
-	// 家南 檬扁拳
-	WSADATA wsaData;
-	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		cout << "WSAStartup failed" << endl;
-		return 0;
-	}
+	int a = 1;
+	bool b = true;
+	float c = 3.14159f;
 
-	// 家南 积己
-	SOCKET CistenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
-	if (CistenSocket == INVALID_SOCKET) {
-		cout << "socket failed" << endl;
-		return 0;
-	}
+	struct
+	{
+		float x;
+		float y;
+	} d[5];
 
-	cout << "Client Start" << endl;
+	msg << a << b << c << d;
 
-	// 家南 楷搬 夸没
-	SOCKADDR_IN serverAddr;
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-	serverAddr.sin_port = htons(9000);
+	a = 99;
+	b = false;
+	c = 99.0f;
 
-	if (::connect(CistenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-		cout << "connect failed" << endl;
-		return 0;
-	}
+	msg >> d >> c >> b >> a;
 
-	// 单捞磐 烹脚
-	while (true) {
-		char sendBuffer[256] = "Test Msg";
-		int sendLen = ::send(CistenSocket, sendBuffer, sizeof(sendBuffer), 0);
-		if (sendLen == SOCKET_ERROR) {
-			cout << "send failed" << endl;
-			break;
-		}
-
-		char recvBuffer[256];
-		int recvLen = ::recv(CistenSocket, recvBuffer, sizeof(recvBuffer), 0);
-		if (recvLen == SOCKET_ERROR) {
-			cout << "recv failed" << endl;
-			break;
-		}
-		else if (recvLen == 0) {
-			cout << "server disconnected" << endl;
-			break;
-		}
-		cout << "recv : " << recvBuffer << endl;
-
-	}
-
+	return 0;
 }
