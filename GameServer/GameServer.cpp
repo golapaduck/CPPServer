18f0199e039fc84847f4f2144b17bf694ea82f37
golapaduck <golapaduck29@naver.com>
2025-05-11 17:42:04 +0900

    Feat: GameServer에 C++11 동시성 예제 추가 및 학습 문서 개선
    - GameServer.cpp에 std::future, std::promise, std::packaged_task 사용 예제 코드 추가
    - C++11 동시성 관련 학습 문서 (C++11 동시성 - future, promise, packaged_task.md) 내용 보강:
        - std::async 기본 실행 정책 주의사항 명시
        - std::future의 get() 특성, 예외 전달, shared_future 개념 추가
        - std::promise의 일회성 사용 강조
        - std::packaged_task의 reset() 기능 설명 추가

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,57 +5,73 @@
 #include <atomic>
 #include <mutex>
 #include <windows.h>
+#include <future>
 
-mutex m;
-queue<int32> q;
 
-HANDLE handle;
-
-void Producer()
+int64 Calculate()
 {
-	while (true)
-	{	
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-		}
+	int64 sum = 0;
 
-		::SetEvent(handle);
-
-		this_thread::sleep_for(std::chrono::milliseconds(100));
+	for (int32 i = 0; i < 100'000; i++)
+	{
+		sum += i;
 	}
+	return sum;
 }
 
-void Consumer()
+void PromiseWorker(std::promise<string>&& promise)
 {
-	while (true)
-	{
-		::WaitForSingleObject(handle, INFINITE);
-		//::ResetEvent(handle);
-
-		unique_lock<mutex> lock(m);
-		if (q.empty() == false)
-		{
-			int32 data = q.front();
-			q.pop();
-			cout << data << endl;
-		}
-	}
+	promise.set_value("Secret Message");
+}
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+	task();
 }
 
 int main()
 {
-	// 커널 오브젝트
-	// Usage Count
-	// Signal / Non-Signal
-	// Auto / Manual
-	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*binitialState*/, NULL);
+	
+	// std::future
+	// 원하는 함수를 비동기적으로 실행
+	{
+		// 1) deferred			-> 지연 실행
+		// 2) async				-> 별도의 쓰레드 추가
+		// 3) deffered | async	-> 알아서
+		std::future<int64> future = std::async(std::launch::async, Calculate);
+	
+		int64 sum = future.get();
+	
+	}
 
-	thread t1(Producer);
-	thread t2(Consumer);
+	// std::promise
+	// 결과물을 promise를 통해 future로 받아줌
+	{
+		std::promise<string> promise;
+		std::future<string> future = promise.get_future();
 
-	t1.join();
-	t2.join();
+		thread t(PromiseWorker, std::move(promise));
 
-	::CloseHandle(handle);
+		string message = future.get();
+		cout << message << endl;
+
+
+		t.join();
+	}
+
+	// std::packaged_task
+	// 원하는 함수의 실행 결과를 packaged_task를 통해 future로 받아줌
+	{
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64> future = task.get_future();
+
+		std::thread t(TaskWorker, std::move(task));
+
+		int64 sum = future.get();
+		cout << sum << endl;
+		t.join();
+	}
+
+	// 일회성 이벤트에 사용할 방식
+	// mutex, Condition Variable까지 사용 안 해도 됨
+	
 }
